d4c97c1ecdafe26950f74898cfd3bea04c4f3a13
samihasamin <samihasamin@hotmail.com>
2025-03-22 00:42:12 -0400

    SCSS Styling for Header component completed for mobile view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,114 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
-import { FaShoppingCart } from "react-icons/fa";
-// import Logo from "../../assets/logo/svg/Color_logo_with_background.svg";
+import { FaShoppingCart, FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+import Logo from "/assets/logos/png/bowl-icon.png";
 
 function Header({ cartItems }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <nav className="navbar">
         <div className="navbar__logo">
-          {/* <Link to="/">
+          <Link to="/">
             <img src={Logo} alt="EthnicBite Logo" />
-          </Link> */}
+          </Link>
         </div>
-        <ul className="navbar__links-left">
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/getstarted">Get Started</Link>
-          </li>
-        </ul>
-        <ul className="navbar__links-right">
-          <Link to="/become-chef" className="navbar__cta">
-            Become a Chef
-          </Link>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup" className="navbar__signup">
-            Sign Up
-          </Link>
-          <Link to="/cart" className="navbar__cart">
-            <FaShoppingCart size={20} />
-            <span className="navbar__cart-count">{cartItems.length}</span>
-          </Link>
-        </ul>
+
+        {/* Mobile Links */}
+
+        {menuOpen && (
+          <div className="navbar__menu">
+            <ul className="navbar__menu-itmes">
+              <li>
+                <Link
+                  className="navbar__menu-items-aboutus navbar__menu-link"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navbar__menu-items-getstarted navbar__menu-link"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Get Started
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navbar__menu-items-cta navbar__menu-link"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Become a Chef
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navbar__menu-items-login navbar__menu-link"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Log In
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navbar__menu-items-signup navbar__menu-link"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {/* Desktop and Tablet links */}
+
+        <div className="navbar__links">
+          <ul className="navbar__links-left">
+            <li>
+              <Link className="navbar__links-left-aboutus">About Us</Link>
+            </li>
+            <li>
+              <Link className="navbar__links-left-getstarted">Get Started</Link>
+            </li>
+          </ul>
+          <ul className="navbar__links-right">
+            <li>
+              <Link className="navbar__links-right-cta">Become a Chef</Link>
+            </li>
+            <li>
+              <Link>Log In</Link>
+            </li>
+            <li>
+              <Link className="navbar__links-right-cta-signup">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar__controls">
+          <div className="navbar__controls-cart">
+            <Link
+              className="navbar__controls-cart-link"
+              onClick={() => setMenuOpen(false)}
+            >
+              <FaShoppingCart size={20} />
+              {cartItems.length > 0 && (
+                <span className="navbar__cart-count">{cartItems.length}</span>
+              )}
+            </Link>
+          </div>
+
+          <button
+            className="navbar__controls-hamburger"
+            onClick={() => setMenuOpen((prevState) => !prevState)}
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </nav>
     </header>
   );
