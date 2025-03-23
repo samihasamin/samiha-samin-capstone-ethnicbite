3bc6c2478e7a7be304c626c98f22bca2a750b2fb
samihasamin <samihasamin@hotmail.com>
2025-03-23 06:09:43 -0400

    Fix infinite loop error

--- src/components/CuisineCarousel/CuisineCarousel.jsx
+++ src/components/CuisineCarousel/CuisineCarousel.jsx
@@ -5,10 +5,23 @@ import { useState, useEffect, useRef } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import LeftArrow from "../../icons/arrow-left.svg?react";
+import RightArrow from "../../icons/arrow-right.svg?react";
 
 function CuisineCarousel() {
   const [cuisines, setCuisines] = useState([]);
-  const sliderRef = useRef(null);
+
+  const PrevArrow = (props) => (
+    <div className="custom-arrow prev" onClick={props.onClick}>
+      <LeftArrow className="custom-arrow-icon" />
+    </div>
+  );
+
+  const NextArrow = (props) => (
+    <div className="custom-arrow next" onClick={props.onClick}>
+      <RightArrow className="custom-arrow-icon" />
+    </div>
+  );
 
   const settings = {
     dots: false,
@@ -16,18 +29,35 @@ function CuisineCarousel() {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    centerMode: false,
+    variableWidth: true,
     arrows: true,
+    cssEase: "linear",
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 3000, // desktop
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
+          infinite: true,
+          variableWidth: true,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 1024, // tablet
         settings: {
           slidesToShow: 3,
+          infinite: true,
+          variableWidth: true,
+        },
+      },
+      {
+        breakpoint: 768, // mobile
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          variableWidth: true,
         },
       },
     ],
