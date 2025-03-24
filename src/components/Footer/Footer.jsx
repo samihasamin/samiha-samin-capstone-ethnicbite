49545c8c514a3c9751e9ad2012008f8cf416098f
samihasamin <samihasamin@hotmail.com>
2025-03-24 13:17:58 -0400

    Complete footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,30 +5,37 @@ function Footer() {
     <footer className="footer">
       <div className="footer__top">
         <div className="footer__top-logo">
-          <img src="/assets/logo/color-logo.svg" alt="EthnicBite Logo" />
+          <img
+            src="/assets/logos/png/ethnicbite-logo-footer.png"
+            alt="EthnicBite Logo"
+          />
           <p>Brining authentic, traditional home flavors to all of Canada!</p>
         </div>
-        <div className="footer__top-mainpages">
-          <h4>Main Pages</h4>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Explore Our Chefs</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div className="footer__top-support">
-          <h4>Support & Legal</h4>
-          <li>FAQ</li>
-          <li>Privacy Policy</li>
-          <li>Terms and Conditions</li>
-          <li>Customer Service</li>
-        </div>
-        <div className="footer__top-contact">
-          <h4>Contact Details</h4>
-          <p>support@ehnicbite.ca</p>
-          <p>123 Maple Leaf Avenue,</p>
-          <p>Halifax, Nova Scotia, B3L 2R7</p>
+        <div className="footer__top-links">
+          <div className="footer__top-links__mainpages">
+            <h4>Main Pages</h4>
+            <ul>
+              <li>Home</li>
+              <li>About Us</li>
+              <li>Explore Our Chefs</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="footer__top-links__support">
+            <h4>Support & Legal</h4>
+            <ul>
+              <li>FAQ</li>
+              <li>Privacy Policy</li>
+              <li>Terms and Conditions</li>
+              <li>Customer Service</li>
+            </ul>
+          </div>
+          <div className="footer__top-links__contact">
+            <h4>Contact Details</h4>
+            <p>support@ehnicbite.ca</p>
+            <p>123 Maple Leaf Avenue,</p>
+            <p>Halifax, Nova Scotia, B3L 2R7</p>
+          </div>
         </div>
       </div>
       <div className="footer__copyright">
